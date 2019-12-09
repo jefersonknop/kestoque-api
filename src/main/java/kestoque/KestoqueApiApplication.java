07e0c5d9e5ab035e3590da732b07cbe3b6176bfd
jefersonknop <jefersonknop@gmail.com>
2019-12-09 14:47:17 -0300

    commit inicial

---
+++ src/main/java/kestoque/KestoqueApiApplication.java
@@ -0,0 +1,13 @@
+package kestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KestoqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KestoqueApiApplication.class, args);
+	}
+
+}
